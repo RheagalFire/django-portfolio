f2a2c2881dd08450457f66a24defd054f8ff6e87
Aarish Alam <arishalam121@gmail.com>
2021-11-07 23:02:58 +0530

    Update settings.py

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'resume_app.urls'
