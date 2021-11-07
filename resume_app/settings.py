f9f11c555cbd0fadfd65827132230cdf202b56dc
Aarish Alam <arishalam121@gmail.com>
2021-11-07 21:46:32 +0530

    Update settings.py

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -117,20 +117,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_URL='/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
-]
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
